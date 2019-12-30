228650df86f6a8025d3a6332baf90964a2849e2b
Extrasklep <extrasklep@firemail.cc>
2019-12-30 18:33:19 +0100

    Better itoa

--- rt_player/main.cpp
+++ rt_player/main.cpp
@@ -53,33 +53,10 @@ unsigned long written_samples=0;
 
 unsigned char* memblock;
 
-//aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-void reverse(char s[]) {
-    int i, j;
-    char c;
-    
-    for (i = 0, j = strlen(s)-1; i<j; i++, j--) {
-        c = s[i];
-        s[i] = s[j];
-        s[j] = c;
-    }
+void itoa(int n, char* s) {
+    std::string ss = std::to_string(n);
+    strcpy(s,ss.c_str());
 }  
-void itoa(int n, char s[]) {
-    int i, sign;
-    
-    if ((sign = n) < 0)  /* record sign */
-        n = -n;          /* make n positive */
-        i = 0;
-    do {       /* generate digits in reverse order */
-        s[i++] = n % 10 + '0';   /* get next digit */
-    } while ((n /= 10) > 0);     /* delete it */
-    if (sign < 0)
-        s[i++] = '-';
-    s[i] = '\0';
-    reverse(s);
-}  
-//-----------------------------------------whatever
-
 
 char* mString;
 char* secondsToMString(unsigned int sec) {
