e37d6e2cfca4655f427bd600cc4be2cc045c6930
Extrasklep <extrasklep@firemail.cc>
2020-01-20 20:48:01 +0100

    Minor improvements

--- rt_player/main.cpp
+++ rt_player/main.cpp
@@ -43,7 +43,7 @@ unsigned int  HEAD2_track_panning     [8] = {0,0,0,0,0,0,0,0};
 //HEAD3
 unsigned int  HEAD3_num_channels;
 
-int16_t* PCM_samples[16];
+int16_t* PCM_samples[16]; //unused here
 
 int16_t* PCM_buffer[16];
 
@@ -51,6 +51,7 @@ unsigned long written_samples=0;
 
 #include "../brstm.h" //must be included after this stuff
 
+//BRSTM file memblock
 unsigned char* memblock;
 
 void itoa(int n, char* s) {
@@ -83,6 +84,7 @@ char* secondsToMString(unsigned int sec) {
     return mString;
 }
 
+//stolen
 char getch(void) {
     char buf = 0;
     struct termios old = {0};
@@ -105,59 +107,55 @@ char getch(void) {
 }
 
 long playback_current_sample=0;
-unsigned int Channels=0;
 int current_track=0;
 unsigned long playback_seconds=0;
 unsigned long total_seconds=0;
 bool stop_playing=0;
 bool paused=0;
-unsigned int update_display_i=0;
 
 //RtAudio callback
 int RtAudioCb( void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames, double streamTime, RtAudioStreamStatus status, void *userData) {
-    unsigned int i, c;
+    unsigned int i;
     double *buffer = (double *) outputBuffer;
-    if (status) {std::cout << "Stream underflow detected!\n";}
+    //if(status) {std::cout << "Stream underflow detected!\n";}
     
-    //Display
+    //Update the display
     playback_seconds=playback_current_sample/HEAD1_sample_rate;
-    //std::cout << "\rCurrent time: " << secondsToMString(playback_seconds) << " Buffer frames: " << nBufferFrames << " Channels: " << Channels << " Played samples: " << playback_current_sample << "              ";
-    if(update_display_i>0) {
-        update_display_i=0; 
-        std::cout << '\r';
-        if(paused) {std::cout << "Paused ";}
-        std::cout /*<< "\e[?25l"*/ << "(" << secondsToMString(playback_seconds) << "/" << secondsToMString(total_seconds) << " Track: " << current_track+1 << ") (< >:Seek /\\ \\/:Switch track):\033[0m                           \r" /*<< "\e[?25h"*/;
-    }
-    update_display_i++;
+    std::cout << '\r';
+    if(paused) {std::cout << "Paused ";}
+    std::cout << "(" << secondsToMString(playback_seconds) << "/" << secondsToMString(total_seconds) << " Track: " << current_track+1 << ") (< >:Seek /\\ \\/:Switch track):\033[0m                           \r";
     
     //Get buffer and write data
+    unsigned char ch1id = HEAD2_track_lchannel_id[current_track];
+    unsigned char ch2id = HEAD3_num_channels > 1 ? HEAD2_track_rchannel_id[current_track] : ch1id;
+    
     if(!paused) {
         brstm_getbuffer(memblock,playback_current_sample,nBufferFrames,true);
         int ioffset=0;
         for (i=0;i<nBufferFrames;i+=1) {
-            for(c=0;c<Channels;c++) {
-                unsigned char tchannel;
-                if(c==0) {tchannel=HEAD2_track_lchannel_id[current_track];}
-                else {tchannel=HEAD2_track_rchannel_id[current_track];}
-                *buffer++ = (double)PCM_buffer[tchannel][i+ioffset]/(double)32768;
-            }
+            *buffer++ = (double)PCM_buffer[ch1id][i+ioffset] / 32768;
+            *buffer++ = (double)PCM_buffer[ch2id][i+ioffset] / 32768;
+            
             playback_current_sample++;
-            if(playback_current_sample>HEAD1_total_samples) {
+            if(playback_current_sample > HEAD1_total_samples) {
                 if(HEAD1_loop) {
                     playback_current_sample=HEAD1_loop_start;
                     //refill buffer
                     brstm_getbuffer(memblock,playback_current_sample,nBufferFrames,true);
                     ioffset-=i;
-                } else {stop_playing=1; return 1;}
-            }
-        }
-    } else {
-            for(i=0;i<nBufferFrames;i+=1) {
-                for(c=0;c<Channels;c++) {
-                    *buffer++ = 0;
+                } else {
+                    stop_playing=1; 
+                    return 1;
                 }
             }
         }
+    } else {
+        //player is paused
+        for(i=0;i<nBufferFrames;i+=1) {
+            *buffer++ = 0;
+            *buffer++ = 0;
+        }
+    }
     return 0;
 }
 
@@ -169,7 +167,6 @@ const char* helpString1 = " [file to open] [options...]\nOptions:\n-v - Verbose 
 const char* opts[] = {"-v"};
 //____________________________________
 bool verb=0;
-bool saveFile=0;
 
 int main( int argc, char* args[] ) {
     if(argc<2) {
@@ -177,7 +174,8 @@ int main( int argc, char* args[] ) {
         return 0;
     }
     const char* outputName;
-    //check user options
+    //Check user arguments
+    //TODO replace this with something better and cleaner?
     for(unsigned int i=2;i<argc;i++) {
         char* currentArg=args[i];
         int vOpt=-1;
@@ -191,12 +189,9 @@ int main( int argc, char* args[] ) {
             }
         }
         if(vOpt==0) {verb=1;}
-        else if(vOpt==1) {if(argc-1>i) {outputName=args[++i]; saveFile=1;}}
     }
     
-    //file data
-    
-    //read the file
+    //Read the file
     if(verb) {std::cout << "Reading file " << args[1];}
     std::streampos fsize;
     std::ifstream file (args[1], std::ios::in|std::ios::binary|std::ios::ate);
@@ -206,18 +201,17 @@ int main( int argc, char* args[] ) {
         file.seekg (0, std::ios::beg);
         file.read ((char*)memblock, fsize);
         if(verb) {std::cout << " size " << fsize << '\n';}
-        //file.close();
-        //delete[] memblock;
+        file.close();
     } else {std::cout << "\nUnable to open file\n"; return 255;}
     
-    //read the brstm
+    //Read the BRSTM headers
     unsigned char result=readBrstm(memblock,verb,false);
     if(result>127) {
         std::cout << "Error.\n";
         return result;
     }
     
-    //initialize rtaudio
+    //Initialize rtaudio
     RtAudio dac;
     if (dac.getDeviceCount()<1) {
         std::cout << "No audio device found.\n";
@@ -226,15 +220,12 @@ int main( int argc, char* args[] ) {
     RtAudio::StreamParameters parameters;
     RtAudio::StreamOptions options;
     parameters.deviceId = dac.getDefaultOutputDevice();
-    if(HEAD3_num_channels<2) {Channels=1;}
-    else {Channels=2;}
-    parameters.nChannels = Channels;
+    parameters.nChannels = 2;
     parameters.firstChannel = 0;
-    options.streamName="BRSTM";
+    options.streamName = "BRSTM";
     unsigned int sampleRate = HEAD1_sample_rate;
     unsigned int bufferFrames = 256; // 256 sample frames
     double data[2];
-    if(verb) {std::cout << "Playback channels: " << parameters.nChannels << '\n';}
     try {
         dac.openStream( &parameters, NULL, RTAUDIO_FLOAT64, sampleRate, &bufferFrames, &RtAudioCb, (void *)&data, &options);
         dac.startStream();
@@ -250,26 +241,26 @@ int main( int argc, char* args[] ) {
     while(stop_playing==0) {
         input=getch();
         if(input=='\033') {
-        getch();
-        input=getch();
-        switch(input) {
-            case 'A': /*U*/ current_track++; if(current_track>=HEAD2_num_tracks) {current_track=HEAD2_num_tracks-1;} /*std::cout << "UP\n";*/ break;
-            case 'B': /*D*/ current_track--; if(current_track<0) {current_track=0;} /*std::cout << "DOWN\n";*/ break;
-            case 'C': /*R*/ playback_current_sample+=HEAD1_sample_rate; if(playback_current_sample>HEAD1_total_samples) {playback_current_sample=HEAD1_total_samples;} /*std::cout << "RIGHT\n";*/ break;
-            case 'D': /*L*/ playback_current_sample-=HEAD1_sample_rate; if(playback_current_sample<0) {playback_current_sample=0;} /*std::cout << "LEFT\n";*/ break;
-        }
+            getch();
+            input=getch();
+            switch(input) {
+                case 'A': /*U*/ current_track++; if(current_track>=HEAD2_num_tracks) {current_track=HEAD2_num_tracks-1;} break;
+                case 'B': /*D*/ current_track--; if(current_track<0) {current_track=0;} break;
+                case 'C': /*R*/ playback_current_sample+=HEAD1_sample_rate; if(playback_current_sample>HEAD1_total_samples) {playback_current_sample=HEAD1_total_samples;} break;
+                case 'D': /*L*/ playback_current_sample-=HEAD1_sample_rate; if(playback_current_sample<0) {playback_current_sample=0;} break;
+            }
         } else switch(input) {
-            /*case 'w': case 'W': std::cout << "W UP\n"; break;
-            case 's': case 'S': std::cout << "S DOWN\n"; break;
-            case 'a': case 'A': std::cout << "A LEFT\n"; break;
-            case 'd': case 'D': std::cout << "D RIGHT\n"; break;*/
-            case ' ': paused=!paused; /*std::cout << "SPACE\n";*/ break;
+            //reserved for more features in the future?
+            /*case 'w': case 'W': break;
+            case 's': case 'S': break;
+            case 'a': case 'A': break;
+            case 'd': case 'D': break;*/
+            case ' ': paused=!paused; break;
+            case 'q': stop_playing = 1;
         }
         //std::this_thread::sleep_for(std::chrono::milliseconds(5));
     }
     std::cout << '\n';
-    //std::cin.get( input );
-    
     
     try {
         // Stop the stream
