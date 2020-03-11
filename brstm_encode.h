1344d7cc63cfa5c0fac7b02e87e572a823538fe8
Extrasklep <extrasklep@firemail.cc>
2020-03-11 15:55:46 +0100

    Add brstm_encode function & test encoder program

--- brstm_encode.h
+++ brstm_encode.h
@@ -4,3 +4,12 @@
 
 //This file requires the same variables as brstm.h to be declared in the including file
 
+//unsigned char* brstm_encoded_data;
+//unsigned long  brstm_encoded_data_size;
+//declare in including file
+
+unsigned char brstm_encode() {
+    delete[] brstm_encoded_data;
+    
+    return 0;
+}
---
+++ encoder.cpp
@@ -0,0 +1,125 @@
+//C++ BRSTM encoder test
+//Copyright (C) 2020 Extrasklep
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstdlib>
+#include <cstring>
+
+//brstm stuff
+
+unsigned int  HEAD1_codec; //char
+unsigned int  HEAD1_loop;  //char
+unsigned int  HEAD1_num_channels; //char
+unsigned int  HEAD1_sample_rate;
+unsigned long HEAD1_loop_start;
+unsigned long HEAD1_total_samples;
+unsigned long HEAD1_ADPCM_offset;
+unsigned long HEAD1_total_blocks;
+unsigned long HEAD1_blocks_size;
+unsigned long HEAD1_blocks_samples;
+unsigned long HEAD1_final_block_size;
+unsigned long HEAD1_final_block_samples;
+unsigned long HEAD1_final_block_size_p;
+unsigned long HEAD1_samples_per_ADPC;
+unsigned long HEAD1_bytes_per_ADPC;
+
+unsigned int  HEAD2_num_tracks;
+unsigned int  HEAD2_track_type;
+
+unsigned int  HEAD2_track_num_channels[8] = {0,0,0,0,0,0,0,0};
+unsigned int  HEAD2_track_lchannel_id [8] = {0,0,0,0,0,0,0,0};
+unsigned int  HEAD2_track_rchannel_id [8] = {0,0,0,0,0,0,0,0};
+//type 1 only
+unsigned int  HEAD2_track_volume      [8] = {0,0,0,0,0,0,0,0};
+unsigned int  HEAD2_track_panning     [8] = {0,0,0,0,0,0,0,0};
+//HEAD3
+unsigned int  HEAD3_num_channels;
+
+int16_t* PCM_samples[16];
+
+int16_t* PCM_buffer[16]; //unused in this program
+
+unsigned long written_samples=0;
+
+#include "brstm.h" //must be included after this stuff
+
+unsigned char* brstm_encoded_data;
+unsigned long  brstm_encoded_data_size;
+#include "brstm_encode.h" //must be included after this stuff
+
+//-------------------######### STRINGS
+
+const char* helpString0 = "Usage:\n";
+const char* helpString1 = " [file to open] [options...]\nOptions:\n-o [output file name] - If this is not used the output will not be saved.\n-v - Verbose output\n";
+
+const char* opts[] = {"-v","-o"};
+//____________________________________
+bool verb=0;
+bool saveFile=0;
+
+int main( int argc, char* args[] ) {
+    if(argc<2) {
+        std::cout << helpString0 << args[0] << helpString1;
+        return 0;
+    }
+    const char* outputName;
+    //check user options
+    for(unsigned int i=2;i<argc;i++) {
+        char* currentArg=args[i];
+        int vOpt=-1;
+        for(unsigned int o=0;o<2;o++) {
+            unsigned int matched=0;
+        for(unsigned int s=0;s<strlen(currentArg);s++) {
+            if(args[i][s]==opts[o][s]) {matched++;}
+        }
+            if(matched>=2) {
+                vOpt=o;
+            }
+        }
+        if(vOpt==0) {verb=1;}
+        else if(vOpt==1) {if(argc-1>i) {outputName=args[++i]; saveFile=1;}}
+    }
+    
+    //file data
+    
+    //read the file
+    if(verb) {std::cout << "Reading file " << args[1];}
+    std::streampos fsize;
+    unsigned char * memblock;
+    std::ifstream file (args[1], std::ios::in|std::ios::binary|std::ios::ate);
+    if (file.is_open()) {
+        fsize = file.tellg();
+        memblock = new unsigned char [fsize];
+        file.seekg (0, std::ios::beg);
+        file.read ((char*)memblock, fsize);
+        if(verb) {std::cout << " size " << fsize << '\n';}
+        //file.close();
+        //delete[] memblock;
+    } else {std::cout << "\nUnable to open file\n"; return 255;}
+    
+    
+    //read the brstm
+    unsigned char result=brstm_read(memblock,verb+1,true);
+    if(result>127) {
+        std::cout << "Error.\n";
+        return result;
+    }
+    
+    //encode new brstm
+    if(saveFile) {
+        std::cout << "Saving file to " << outputName << "...\n";
+        
+        std::ofstream ofile (outputName,std::ios::out|std::ios::binary|std::ios::trunc);
+        if(ofile.is_open()) {
+            unsigned char res = brstm_encode();
+            ofile.write((char*)brstm_encoded_data,brstm_encoded_data_size);
+            ofile.close();
+        } else {std::cout << "\nUnable to open file\n"; return 255;}
+    }
+    
+    brstm_close();
+    
+    return 0;
+}
