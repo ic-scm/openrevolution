170218b06eaad158236ff7ad51425281b91d9fc3
Extrasklep <extrasklep@firemail.cc>
2020-03-11 14:58:16 +0100

    Add brstm_encode.h

---
+++ brstm_encode.h
@@ -0,0 +1,6 @@
+//C++ BRSTM encoder
+//Copyright (C) 2020 Extrasklep
+//https://github.com/Extrasklep/brstm
+
+//This file requires the same variables as brstm.h to be declared in the including file
+
