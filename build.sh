b89d444ff3f94aa63b5c4bcbeccf612109790db1
Extrasklep <extrasklep@firemail.cc>
2020-03-14 13:22:24 +0100

    Update build script

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
+echo "Building decoder..."
 g++ -O2 main.cpp -o brstm
+echo "Building player..."
 g++ -O2 rt_player/main.cpp -o rt_player/brstm_rt -lrtaudio
-
+echo "Building encoder..."
+g++ -O2 encoder/encoder.cpp -o brstm_encoder
+echo "Building reencoder..."
+g++ -O2 encoder/reencoder.cpp -o brstm_reencoder
