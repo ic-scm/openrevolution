6cfbb21335c76cde37589acf1eb72a2f29814b99
Extrasklep <extrasklep@firemail.cc>
2020-04-05 23:04:11 +0200

    Use decode block function in BWAV reader and update that function

--- src/lib/audio_decoder.h
+++ src/lib/audio_decoder.h
@@ -5,94 +5,98 @@
 
 //dataType will be 1 for disk streaming mode (fileData will be null) so brstm_getblock
 //will know to do disk streaming stuff instead of just getting a slice of fileData
+void brstm_decode_block(Brstm* brstmi,unsigned long b,unsigned int c,const unsigned char* fileData,bool dataType,int16_t** decodeDest,unsigned long decodeDestOff) {
+    unsigned long posOffset=0+(brstmi->blocks_size*c);
+    unsigned long outputPos = 0; //position in decoded PCM samples output array
+    
+    unsigned long c_writtensamples = 0;
+    
+    posOffset+=b*(brstmi->blocks_size*brstmi->num_channels);
+    
+    //Read block
+    unsigned int currentBlockSize    = brstmi->blocks_size;
+    unsigned int currentBlockSamples = brstmi->blocks_samples;
+    //Final block
+    if(b==brstmi->total_blocks-1) {
+        currentBlockSize    = brstmi->final_block_size;
+        currentBlockSamples = brstmi->final_block_samples;
+    }
+    if(b>=brstmi->total_blocks-1 && c>0) {
+        //Go back to the previous position
+        posOffset-=brstmi->blocks_size*brstmi->num_channels;
+        //Go to the next block in position of first channel
+        posOffset+=brstmi->blocks_size*(brstmi->num_channels-c);
+        //Jump to the correct channel in the final block
+        posOffset+=brstmi->final_block_size_p*c;
+    }
+    //Get data from just the current block
+    //This function exists in the including file (brstm.h)
+    unsigned char* blockData = brstm_getblock(fileData,dataType,brstmi->audio_offset+posOffset,currentBlockSize);
+    
+    //Decode the audio
+    if(brstmi->codec == 0) {
+        //8 bit PCM
+        for(unsigned long sampleIndex=0;sampleIndex<currentBlockSamples;sampleIndex++) {
+            decodeDest[c][decodeDestOff+(outputPos++)] = ((int16_t)blockData[sampleIndex]-128)*256;
+            c_writtensamples++;
+        }
+    } else if(brstmi->codec == 1) {
+        //16 bit PCM
+        for(unsigned long sampleIndex=0;sampleIndex<currentBlockSamples;sampleIndex++) {
+            decodeDest[c][decodeDestOff+(outputPos++)] = brstm_getSliceAsInt16Sample(blockData,sampleIndex*2,brstmi->BOM);
+            c_writtensamples++;
+        }
+    } else if(brstmi->codec == 2) {
+        //4 bit DSPADPCM
+        const unsigned char ps = blockData[0];
+        const   signed int  yn1 = brstmi->ADPCM_hsamples_1[c][b], yn2 = brstmi->ADPCM_hsamples_2[c][b];
+        
+        //Magic adapted from brawllib's ADPCMState.cs
+        signed int 
+        cps = ps,
+        cyn1 = yn1,
+        cyn2 = yn2;
+        unsigned long dataIndex = 0;
+        
+        int16_t* coefs = brstmi->ADPCM_coefs[c];
+        
+        for (unsigned long sampleIndex=0;sampleIndex<currentBlockSamples;) {
+            long outSample = 0;
+            if (sampleIndex % 14 == 0) {
+                cps = blockData[dataIndex++];
+            }
+            if ((sampleIndex++ & 1) == 0) {
+                outSample = blockData[dataIndex] >> 4;
+            } else {
+                outSample = blockData[dataIndex++] & 0x0f;
+            }
+            if (outSample >= 8) {
+                outSample -= 16;
+            }
+            const long scale = 1 << (cps & 0x0f);
+            const long cIndex = (cps >> 4) << 1;
+            
+            outSample = (0x400 + ((scale * outSample) << 11) + coefs[brstm_clamp(cIndex, 0, 15)] * cyn1 + coefs[brstm_clamp(cIndex + 1, 0, 15)] * cyn2) >> 11;
+            
+            cyn2 = cyn1;
+            cyn1 = brstm_clamp(outSample, -32768, 32767);
+            
+            decodeDest[c][decodeDestOff+(outputPos++)] = cyn1;
+            c_writtensamples++;
+        }
+    }
+    
+    posOffset+=brstmi->blocks_size*brstmi->num_channels;
+}
+
+//Default decoding into PCM_blockbuffer
 void brstm_decode_block(Brstm* brstmi,unsigned long b,const unsigned char* fileData,bool dataType) {
     for(unsigned int c=0;c<brstmi->num_channels;c++) {
         //Create new array of samples for the current channel
         delete[] brstmi->PCM_blockbuffer[c];
         brstmi->PCM_blockbuffer[c] = new int16_t[brstmi->blocks_samples];
         
-        unsigned long posOffset=0+(brstmi->blocks_size*c);
-        unsigned long outputPos = 0; //position in PCM block samples output array
-        
-        unsigned long c_writtensamples = 0;
-        
-        posOffset+=b*(brstmi->blocks_size*brstmi->num_channels);
-        
-        //Read block
-        unsigned int currentBlockSize    = brstmi->blocks_size;
-        unsigned int currentBlockSamples = brstmi->blocks_samples;
-        //Final block
-        if(b==brstmi->total_blocks-1) {
-            currentBlockSize    = brstmi->final_block_size;
-            currentBlockSamples = brstmi->final_block_samples;
-        }
-        if(b>=brstmi->total_blocks-1 && c>0) {
-            //Go back to the previous position
-            posOffset-=brstmi->blocks_size*brstmi->num_channels;
-            //Go to the next block in position of first channel
-            posOffset+=brstmi->blocks_size*(brstmi->num_channels-c);
-            //Jump to the correct channel in the final block
-            posOffset+=brstmi->final_block_size_p*c;
-        }
-        //Get data from just the current block
-        //This function exists in the including file (brstm.h)
-        unsigned char* blockData = brstm_getblock(fileData,dataType,brstmi->audio_offset+posOffset,currentBlockSize);
-        
-        //Decode the audio
-        if(brstmi->codec == 0) {
-            //8 bit PCM
-            for(unsigned long sampleIndex=0;sampleIndex<currentBlockSamples;sampleIndex++) {
-                brstmi->PCM_blockbuffer[c][outputPos++] = ((int16_t)blockData[sampleIndex]-128)*256;
-                c_writtensamples++;
-            }
-        } else if(brstmi->codec == 1) {
-            //16 bit PCM
-            for(unsigned long sampleIndex=0;sampleIndex<currentBlockSamples;sampleIndex++) {
-                brstmi->PCM_blockbuffer[c][outputPos++] = brstm_getSliceAsInt16Sample(blockData,sampleIndex*2,brstmi->BOM);
-                c_writtensamples++;
-            }
-        } else if(brstmi->codec == 2) {
-            //4 bit DSPADPCM
-            const unsigned char ps = blockData[0];
-            const   signed int  yn1 = brstmi->ADPCM_hsamples_1[c][b], yn2 = brstmi->ADPCM_hsamples_2[c][b];
-            
-            //Magic adapted from brawllib's ADPCMState.cs
-            signed int 
-            cps = ps,
-            cyn1 = yn1,
-            cyn2 = yn2;
-            unsigned long dataIndex = 0;
-            
-            int16_t* coefs = brstmi->ADPCM_coefs[c];
-            
-            for (unsigned long sampleIndex=0;sampleIndex<currentBlockSamples;) {
-                long outSample = 0;
-                if (sampleIndex % 14 == 0) {
-                    cps = blockData[dataIndex++];
-                }
-                if ((sampleIndex++ & 1) == 0) {
-                    outSample = blockData[dataIndex] >> 4;
-                } else {
-                    outSample = blockData[dataIndex++] & 0x0f;
-                }
-                if (outSample >= 8) {
-                    outSample -= 16;
-                }
-                const long scale = 1 << (cps & 0x0f);
-                const long cIndex = (cps >> 4) << 1;
-                
-                outSample = (0x400 + ((scale * outSample) << 11) + coefs[brstm_clamp(cIndex, 0, 15)] * cyn1 + coefs[brstm_clamp(cIndex + 1, 0, 15)] * cyn2) >> 11;
-                
-                cyn2 = cyn1;
-                cyn1 = brstm_clamp(outSample, -32768, 32767);
-                
-                brstmi->PCM_blockbuffer[c][outputPos++] = cyn1;
-                c_writtensamples++;
-            }
-        }
-        
-        brstmi->PCM_blockbuffer_currentBlock = b;
-        
-        posOffset+=brstmi->blocks_size*brstmi->num_channels;
+        brstm_decode_block(brstmi,b,c,fileData,dataType,brstmi->PCM_blockbuffer,0);
     }
+    brstmi->PCM_blockbuffer_currentBlock = b;
 }
--- src/lib/d_formats/bwav.h
+++ src/lib/d_formats/bwav.h
@@ -43,13 +43,14 @@ unsigned char brstm_formats_read_bwav(Brstm* brstmi,const unsigned char* fileDat
     if(brstmi->num_tracks>1 && debugLevel>=0) {std::cout << "Warning: Tracks are assumed\n";}
     
     //Audio
-    //This is stupid copied code but works for now and eventually I plan to move the decoder into a function
+    //Create history samples
+    for(unsigned char c=0;c<brstmi->num_channels;c++) {
+        brstmi->ADPCM_hsamples_1[c] = new int16_t[1];
+        brstmi->ADPCM_hsamples_2[c] = new int16_t[1];
+        brstmi->ADPCM_hsamples_1[c][0] = 0;
+        brstmi->ADPCM_hsamples_2[c][0] = 0;
+    }
     if(decodeAudio) {
-        //Read the ADPCM data
-        unsigned long decoded_samples=0;
-        
-        unsigned long posOffset=0;
-        
         for(unsigned int c=0;c<brstmi->num_channels;c++) {
             //Create new array of samples for the current channel
             switch(decodeAudio) {
@@ -57,79 +58,26 @@ unsigned char brstm_formats_read_bwav(Brstm* brstmi,const unsigned char* fileDat
                 case 2: brstmi->ADPCM_data [c] = new unsigned char[brstmi->blocks_size]; break;
             }
             
-            posOffset=0+(brstmi->blocks_size*c);
+            unsigned long posOffset=0+(brstmi->blocks_size*c);
             unsigned long outputPos = 0; //position in PCM samples or ADPCM data output array
+            
             for(unsigned long b=0;b<brstmi->total_blocks;b++) {
-                //Read every block
-                unsigned int currentBlockSize    = brstmi->blocks_size;
-                unsigned int currentBlockSamples = brstmi->blocks_samples;
-                //Final block
-                if(b==brstmi->total_blocks-1) {
-                    currentBlockSize    = brstmi->final_block_size;
-                    currentBlockSamples = brstmi->final_block_samples;
-                }
-                if(b>=brstmi->total_blocks-1 && c>0) {
-                    //Go back to the previous position
-                    posOffset-=brstmi->blocks_size*brstmi->num_channels;
-                    //Go to the next block in position of first channel
-                    posOffset+=brstmi->blocks_size*(brstmi->num_channels-c);
-                    //Jump to the correct channel in the final block
-                    posOffset+=brstmi->final_block_size_p*c;
-                }
-                //Get data from just the current block
-                unsigned char* blockData = brstm_getSlice(fileData,brstmi->audio_offset+posOffset,currentBlockSize);
-                
                 if(decodeAudio == 1) {
-                    if(brstmi->codec == 2) {
-                        //Decode 4 bit ADPCM
-                        const unsigned char ps = blockData[0];
-                        const   signed int  yn1 = 0, yn2 = 0;
-                        
-                        //Magic adapted from brawllib's ADPCMState.cs
-                        signed int 
-                        cps = ps,
-                        cyn1 = yn1,
-                        cyn2 = yn2;
-                        unsigned long dataIndex = 0;
-                        
-                        int16_t* coefs = brstmi->ADPCM_coefs[c];
-                        
-                        for (unsigned long sampleIndex=0;sampleIndex<currentBlockSamples;) {
-                            long outSample = 0;
-                            if (sampleIndex % 14 == 0) {
-                                cps = blockData[dataIndex++];
-                            }
-                            if ((sampleIndex++ & 1) == 0) {
-                                outSample = blockData[dataIndex] >> 4;
-                            } else {
-                                outSample = blockData[dataIndex++] & 0x0f;
-                            }
-                            if (outSample >= 8) {
-                                outSample -= 16;
-                            }
-                            const long scale = 1 << (cps & 0x0f);
-                            const long cIndex = (cps >> 4) << 1;
-                            
-                            outSample = (0x400 + ((scale * outSample) << 11) + coefs[brstm_clamp(cIndex, 0, 15)] * cyn1 + coefs[brstm_clamp(cIndex + 1, 0, 15)] * cyn2) >> 11;
-                            
-                            cyn2 = cyn1;
-                            cyn1 = brstm_clamp(outSample, -32768, 32767);
-                            
-                            brstmi->PCM_samples[c][outputPos++] = cyn1;
-                            decoded_samples++;
-                        }
-                    } else if(brstmi->codec == 1) {
-                        for(unsigned long s=0;s<currentBlockSamples;s++) {
-                            brstmi->PCM_samples[c][outputPos++] = brstm_getSliceAsInt16Sample(fileData,brstmi->audio_offset+posOffset+s*2,BOM);
-                            decoded_samples++;
-                        }
-                    } else {
-                        if(debugLevel>=0) {std::cout << "Unsupported codec.\n";}
-                        return 220;
-                    }
+                    //Decode audio normally
+                    brstm_decode_block(brstmi,b,c,fileData,0,brstmi->PCM_samples,b*brstmi->blocks_samples);
                 } else {
+                    //Write raw data
+                    //Get block size
+                    unsigned int currentBlockSize    = brstmi->blocks_size;
+                    unsigned int currentBlockSamples = brstmi->blocks_samples;
+                    //Final block
+                    if(b==brstmi->total_blocks-1) {
+                        currentBlockSize    = brstmi->final_block_size;
+                        currentBlockSamples = brstmi->final_block_samples;
+                    }
+                    //Get data from just the current block
+                    unsigned char* blockData = brstm_getSlice(fileData,brstmi->audio_offset+posOffset,currentBlockSize);
                     if(brstmi->codec == 2) {
-                        //Write raw data to ADPCM_data
                         for(unsigned int i=0; i<currentBlockSize; i++) {
                             brstmi->ADPCM_data[c][outputPos++] = blockData[i];
                         }
@@ -138,19 +86,11 @@ unsigned char brstm_formats_read_bwav(Brstm* brstmi,const unsigned char* fileDat
                         return 220;
                     }
                 }
-                
                 posOffset+=brstmi->blocks_size*brstmi->num_channels;
             }
         }
-        if(debugLevel>0) {std::cout << "Decoded PCM samples: " << decoded_samples << '\n';}
     } else {
         if(debugLevel>=0) {std::cout << "Warning: Realtime decoding works like full decoding for this format\n";}
-        for(unsigned char c=0;c<brstmi->num_channels;c++) {
-            brstmi->ADPCM_hsamples_1[c] = new int16_t[1];
-            brstmi->ADPCM_hsamples_2[c] = new int16_t[1];
-            brstmi->ADPCM_hsamples_1[c][0] = 0;
-            brstmi->ADPCM_hsamples_2[c][0] = 0;
-        }
     }
     
     return 0;
