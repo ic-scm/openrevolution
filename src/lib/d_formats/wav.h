be20b204de1cb8168aa649afd2a1c2207c50327e
Extrasklep <ic@smashcustommusic.net>
2020-06-15 16:37:07 +0200

    Add files for WAV

---
+++ src/lib/d_formats/wav.h
@@ -0,0 +1,6 @@
+//OpenRevolution WAV reader
+//Copyright (C) 2020 Extrasklep
+
+unsigned char brstm_formats_read_wav(Brstm* brstmi,const unsigned char* fileData,signed int debugLevel,uint8_t decodeAudio) {
+    return 210;
+}
---
+++ src/lib/e_formats/wav.h
@@ -0,0 +1,6 @@
+//OpenRevolution WAV writer
+//Copyright (C) 2020 Extrasklep
+
+unsigned char brstm_formats_encode_wav(Brstm* brstmi,signed int debugLevel,uint8_t encodeADPCM) {
+    return 210;
+}
