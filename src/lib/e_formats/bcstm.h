718ef4e8e141fb1004f51c59c3afbbf713ad99b8
IC <ic@smashcustommusic.net>
2020-08-26 01:16:00 +0200

    BCSTM/BFSTM encoder: Track information

--- src/lib/e_formats/bcstm.h
+++ src/lib/e_formats/bcstm.h
@@ -36,9 +36,6 @@ unsigned char brstm_formats_multi_encode_bcstm_bfstm(Brstm* brstmi,signed int de
         return 220;
     }
     
-    if(brstmi->num_tracks > 1) {
-        if(debugLevel>=0) std::cout << "Warning: " << eformat_s[eformat] << " cannot store accurate track information\n";
-    }
     
     bool &BOM = brstmi->BOM;
     char spinner = '/';
@@ -67,7 +64,8 @@ unsigned char brstm_formats_multi_encode_bcstm_bfstm(Brstm* brstmi,signed int de
     //Header size, will be written later
     brstm_encoder_writebytes_i(buffer,new unsigned char[2]{0x00,0x00},2,bufpos);
     //Version number? TODO: Research more about it and check if it's different in BFSTM
-    brstm_encoder_writebytes_i(buffer,new unsigned char[4]{0x00,0x00,0x01,0x02},4,bufpos);
+    uint8_t version[4] = {0x00,0x00,0x00,0x02};
+    brstm_encoder_writebytes(buffer,version,4,bufpos);
     //File size, will be written later
     brstm_encoder_writebytes_i(buffer,new unsigned char[4]{0x00,0x00,0x00,0x00},4,bufpos);
     //Number of chunks
@@ -219,7 +217,7 @@ unsigned char brstm_formats_multi_encode_bcstm_bfstm(Brstm* brstmi,signed int de
     brstm_encoder_writebytes_i(buffer,new unsigned char[4]{0x00,0x00,0x00,0x00},4,bufpos);
     //Subchunk references: stream info, track info, channel info
     //Track info is unsupported.
-    bool info_subchunks_to_write[3] = {1,0,1};
+    bool info_subchunks_to_write[3] = {1,1,1};
     const uint16_t info_subchunk_codes[3] = {0x4100,0x0101,0x0101};
     uint32_t info_subchunk_offset_offsets[3] = {0,0,0}; //Reference for writing offsets later.
     uint32_t info_subchunk_offsets[3] = {0,0,0}; //From beginning of INFO chunk content.
@@ -266,38 +264,52 @@ unsigned char brstm_formats_multi_encode_bcstm_bfstm(Brstm* brstmi,signed int de
         //Sample data offset from beginning of DATA chunk content. Will be written later.
         data_audio_offset_offset = bufpos;
         brstm_encoder_writebytes_i(buffer,new unsigned char[4]{0x00,0x00,0x00,0x00},4,bufpos);
-        //Region info size? Seems to be 0x100 in all files.
-        brstm_encoder_writebytes(buffer,brstm_encoder_getByteUint(0x100,2,BOM),2,bufpos);
-        brstm_encoder_writebytes_i(buffer,new unsigned char[2]{0x00,0x00},2,bufpos); //Padding
-        //Region info flag, not used here.
-        brstm_encoder_writebytes(buffer,brstm_encoder_getByteUint(0,2,BOM),2,bufpos);
-        brstm_encoder_writebytes_i(buffer,new unsigned char[2]{0x00,0x00},2,bufpos); //Padding
-        //Region info offset from beginning of REGN chunk content. Unsupported and not used here.
-        brstm_encoder_writebytes_i(buffer,new unsigned char[4]{0xFF,0xFF,0xFF,0xFF},4,bufpos);
+        if(version[2] >= 1) {
+            //Region info size? Seems to be 0x100 in all files.
+            brstm_encoder_writebytes(buffer,brstm_encoder_getByteUint(0x100,2,BOM),2,bufpos);
+            brstm_encoder_writebytes_i(buffer,new unsigned char[2]{0x00,0x00},2,bufpos); //Padding
+            //Region info flag, not used here.
+            brstm_encoder_writebytes(buffer,brstm_encoder_getByteUint(0,2,BOM),2,bufpos);
+            brstm_encoder_writebytes_i(buffer,new unsigned char[2]{0x00,0x00},2,bufpos); //Padding
+            //Region info offset from beginning of REGN chunk content. Unsupported and not used here.
+            brstm_encoder_writebytes_i(buffer,new unsigned char[4]{0xFF,0xFF,0xFF,0xFF},4,bufpos);
+        }
         //"Original" loop start and end in newer files.
-        if(0) {
+        if(version[1] >= 1) {
             brstm_encoder_writebytes(buffer,brstm_encoder_getByteUint(brstmi->loop_start,4,BOM),4,bufpos);
             brstm_encoder_writebytes(buffer,brstm_encoder_getByteUint(brstmi->total_samples,4,BOM),4,bufpos);
         }
     }
     
-    //Track info, unsupported
+    //Track info reference table
+    uint32_t trinfo_struct_beg;
+    uint32_t trinfo_ref_table_offsets[brstmi->num_tracks];
     if(info_subchunks_to_write[1]) {
-        //info_subchunk_offsets[1] = bufpos - (INFOchunkoffset + 8);
-        if(debugLevel>=0) std::cout << "Warning: Writing track information is not currently supported.\n";
+        info_subchunk_offsets[1] = bufpos - (INFOchunkoffset + 8);
+        trinfo_struct_beg = bufpos;
+        //Number of references (tracks)
+        brstm_encoder_writebytes(buffer,brstm_encoder_getByteUint(brstmi->num_tracks,4,BOM),4,bufpos);
+        //References
+        for(unsigned int t=0; t<brstmi->num_tracks; t++) {
+            //Section code
+            brstm_encoder_writebytes(buffer,brstm_encoder_getByteUint(0x4101,2,BOM),2,bufpos);
+            //Padding
+            brstm_encoder_writebytes_i(buffer,new unsigned char[2]{0x00,0x00},2,bufpos);
+            //Offset to track info, will be written later
+            trinfo_ref_table_offsets[t] = bufpos;
+            brstm_encoder_writebytes_i(buffer,new unsigned char[4]{0x00,0x00,0x00,0x00},4,bufpos);
+        }
     }
     
-    //Channel info
-    uint32_t chinfo_ips_offsets[brstmi->num_channels];
-    uint32_t chinfo_lps_offsets[brstmi->num_channels];
+    //Channel info reference table
+    uint32_t chinfo_struct_beg;
+    uint32_t chinfo_ref_table_offsets[brstmi->num_channels];
     if(info_subchunks_to_write[2]) {
         info_subchunk_offsets[2] = bufpos - (INFOchunkoffset + 8);
-        //Channel info reference table.
-        uint32_t struct_beg = bufpos;
+        chinfo_struct_beg = bufpos;
         //Number of references (channels)
         brstm_encoder_writebytes(buffer,brstm_encoder_getByteUint(brstmi->num_channels,4,BOM),4,bufpos);
         //References
-        uint32_t chinfo_ref_table_offsets[brstmi->num_channels];
         for(unsigned int c=0; c<brstmi->num_channels; c++) {
             //Section code
             brstm_encoder_writebytes(buffer,brstm_encoder_getByteUint(0x4102,2,BOM),2,bufpos);
@@ -307,14 +319,59 @@ unsigned char brstm_formats_multi_encode_bcstm_bfstm(Brstm* brstmi,signed int de
             chinfo_ref_table_offsets[c] = bufpos;
             brstm_encoder_writebytes_i(buffer,new unsigned char[4]{0x00,0x00,0x00,0x00},4,bufpos);
         }
-        
+    }
+    
+    //Track information itself
+    if(info_subchunks_to_write[1]) {
+        //Track information
+        for(unsigned int t=0; t<brstmi->num_tracks; t++) {
+            uint32_t trinfo_thistrack_beg = bufpos;
+            //Write the offset to reference table
+            brstm_encoder_writebytes(buffer,brstm_encoder_getByteUint(bufpos-trinfo_struct_beg,4,BOM),4,off=trinfo_ref_table_offsets[t]);
+            if(brstmi->track_desc_type == 1) {
+                //Volume
+                brstm_encoder_writebyte(buffer,brstmi->track_volume[t],bufpos);
+                //Panning
+                brstm_encoder_writebyte(buffer,brstmi->track_panning[t],bufpos);
+            }
+            //Write defaults if the input data's track description type is 0.
+            else {
+                brstm_encoder_writebyte(buffer,0x7F,bufpos);
+                brstm_encoder_writebyte(buffer,0x40,bufpos);
+            }
+            //Padding / unused data
+            brstm_encoder_writebytes_i(buffer,new unsigned char[2]{0x00,0x00},2,bufpos);
+            
+            //Channel index reference
+            //Section code
+            brstm_encoder_writebytes(buffer,brstm_encoder_getByteUint(0x0100,2,BOM),2,bufpos);
+            //Padding
+            brstm_encoder_writebytes_i(buffer,new unsigned char[2]{0x00,0x00},2,bufpos);
+            //Offset to channel index table
+            brstm_encoder_writebytes(buffer,brstm_encoder_getByteUint(bufpos+4-trinfo_thistrack_beg,4,BOM),4,bufpos);
+            
+            //Channel index table
+            //Channel count for track
+            brstm_encoder_writebytes(buffer,brstm_encoder_getByteUint(brstmi->track_num_channels[t],4,BOM),4,bufpos);
+            //Channel IDs
+            brstm_encoder_writebyte(buffer,brstmi->track_lchannel_id[t],bufpos);
+            if(brstmi->track_num_channels[t] == 2) brstm_encoder_writebyte(buffer,brstmi->track_rchannel_id[t],bufpos);
+            //Padding to 0x04
+            while(bufpos % 0x04 != 0) brstm_encoder_writebyte(buffer,0,bufpos);
+        }
+    }
+    
+    //Channel information itself
+    uint32_t chinfo_ips_offsets[brstmi->num_channels];
+    uint32_t chinfo_lps_offsets[brstmi->num_channels];
+    if(info_subchunks_to_write[2]) {
         //Second references...
         uint32_t chinfo_ref2_offsets[brstmi->num_channels];
         uint32_t chinfo_ref2_begs[brstmi->num_channels];
         for(unsigned int c=0; c<brstmi->num_channels; c++) {
             chinfo_ref2_begs[c] = bufpos;
             //Write the offset to first reference table
-            brstm_encoder_writebytes(buffer,brstm_encoder_getByteUint(bufpos-struct_beg,4,BOM),4,off=chinfo_ref_table_offsets[c]);
+            brstm_encoder_writebytes(buffer,brstm_encoder_getByteUint(bufpos-chinfo_struct_beg,4,BOM),4,off=chinfo_ref_table_offsets[c]);
             //Section code
             brstm_encoder_writebytes(buffer,brstm_encoder_getByteUint(0x0300,2,BOM),2,bufpos);
             //Padding
@@ -343,6 +400,8 @@ unsigned char brstm_formats_multi_encode_bcstm_bfstm(Brstm* brstmi,signed int de
             brstm_encoder_writebytes_i(buffer,new unsigned char[2]{0x00,0x00},2,bufpos); //Padding
         }
     }
+    
+    
     
     //Finalize INFO chunk
     //Write subchunk offsets
