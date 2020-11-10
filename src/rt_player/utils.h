c32cd32441a5f5ce9fd853499f767928fe2d9aa2
IC <ic@smashcustommusic.net>
2020-11-10 23:31:34 +0100

    brstm_rt: Disable error messages from getch function

--- src/rt_player/utils.h
+++ src/rt_player/utils.h
@@ -49,19 +49,22 @@ char getch(void) {
     char buf = 0;
     struct termios old = {0};
     fflush(stdout);
-    if(tcgetattr(0, &old) < 0)
-        perror("tcsetattr()");
+    if(tcgetattr(0, &old) < 0) {
+        //perror("tcsetattr()");
+    }
     old.c_lflag &= ~ICANON;
     old.c_lflag &= ~ECHO;
     old.c_cc[VMIN] = 1;
     old.c_cc[VTIME] = 0;
-    if(tcsetattr(0, TCSANOW, &old) < 0)
-        perror("tcsetattr ICANON");
+    if(tcsetattr(0, TCSANOW, &old) < 0) {
+        //perror("tcsetattr ICANON");
+    }
     if(read(0, &buf, 1) < 0)
         perror("read()");
     old.c_lflag |= ICANON;
     old.c_lflag |= ECHO;
-    if(tcsetattr(0, TCSADRAIN, &old) < 0)
-        perror("tcsetattr ~ICANON");
+    if(tcsetattr(0, TCSADRAIN, &old) < 0) {
+        //perror("tcsetattr ~ICANON");
+    }
     return buf;
 }
